e4d4f4b02a219ba5138db0333f9a9b90aff3905b
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-05-28 12:59:30 +0530

    async fetch fn, using mock api

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ let btnTranslate = document.querySelector('#btn-translate')
 let txtInput = document.querySelector('#txt-input')
 let outputDiv = document.querySelector('#output')
 
-btnTranslate.addEventListener('click', () => {
-    outputDiv.innerText = txtInput.value
+let url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
+const makeURL  = (url, text) => url + '?text=' + text
+
+btnTranslate.addEventListener('click', async () => {
+    
+    const res = await fetch(makeURL(url, txtInput.value))
+    const resJSON = await res.json()
+    outputDiv.innerText = resJSON.contents.text
 })
