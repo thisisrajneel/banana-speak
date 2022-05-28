7638d092992d85b5f483549114fcbcfad1fe2077
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-05-28 13:17:12 +0530

    replaced w/ actual url

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ let btnTranslate = document.querySelector('#btn-translate')
 let txtInput = document.querySelector('#txt-input')
 let outputDiv = document.querySelector('#output')
 
-let url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
+// mock server
+//let url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
+
+// actual API
+let url = 'https://api.funtranslations.com/translate/minion.json'
+
+// makes final url using text parameter
 const makeURL  = (url, text) => url + '?text=' + text
 
 btnTranslate.addEventListener('click', async () => {
     
+    // fetching the data from the api and displaying it
     const res = await fetch(makeURL(url, txtInput.value))
     const resJSON = await res.json()
-    outputDiv.innerText = resJSON.contents.text
+    outputDiv.innerText = resJSON.contents.translated
 })
